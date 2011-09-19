a0aae425056148a72da79e72ca02a51193813208
amtal <alex.kropivny@gmail.com>
2011-09-19 03:09:21 -0700

    failing property test - equivalence

--- src/bsp_tests.erl
+++ src/bsp_tests.erl
@@ -4,15 +4,24 @@
 
 go()->proper:module(bsp_tests).
 
+%% Via bsp:new/2, bsp:at/2, assert:
+%%  Indexes are in the right place.
+prop_indexes() ->
+    ?FORALL(Size, ?LET(Dim,integer(1,5),pow2_vector(Dim,4)),
+        ?FORALL(Data, binary(volume(Size)),
+            ?FORALL(Tree, byte_tree(Size,Data), 
+                ?FORALL(Point, rand_pos_vec(Size),
+                    bsp:at(Point,Tree) == binary:at(Data,index(Size,Point)))))).
+
+
 %% Via bsp:new/2, bsp:map/1, and bsp:sparsity/1, assert:
-%%    Reducing the range of the contents tends to simplify the tree.
+%%  Reducing the range of the contents tends to simplify the tree.
 prop_compression() ->
     ?FORALL({Tree,Limit}
            ,{?LET(Dim,integer(1,3),
                 ?LET(Size,pow2_vector(Dim,5),
-                        byte_tree(Size)
-                    )
-             )
+                    ?LET(Data,binary(volume(Size)),
+                        byte_tree(Size,Data))))
             ,integer(0,255)
             }
            ,begin
@@ -38,21 +47,22 @@ pow2_vector(Dims,Limit) ->
         list_to_tuple([{vec,Dims}|Contents])
     end).
 
-byte_tree(Size) ->
-    FlatSize = volume(Size),
-    ?LET(Data,vector(FlatSize,integer(0,255)), begin
-        BinData = list_to_binary(Data),
-        bsp:new(Size, fun(Pos)->binary:at(BinData,index(Size,Pos)) end)
-    end).
+rand_pos_vec(CubeV) -> 
+    [Tag|Es] = tuple_to_list(CubeV),
+    Fst = hd(Es),
+    true = lists:all(fun(X)->X==Fst end, Es),
+    ?LET(Es2, vector(length(Es), integer(0,Fst)), list_to_tuple([Tag|Es2])).
+
+byte_tree(Size,Data) ->
+    bsp:new(Size, fun(Pos)->binary:at(Data,index(Size,Pos)) end).
     
-% where
-    volume(Vec) -> 
-        [_|Vs] = tuple_to_list(Vec),
-        lists:foldl(fun erlang:'*'/2, 1, Vs).
-    index({_,_},{_,I}) -> I;
-    index({_,A,_},{_,I,J}) -> I+J*A; % I'm sure compiler optimizes these~~
-    index({_,A,B,_},{_,I,J,K}) -> I+J*A+K*A*B;
-    index({_,A,B,C,_},{_,I,J,K,L}) -> I+J*A+K*A*B+L*A*B*C;
-    index({_,A,B,C,D,_},{_,I,J,K,L,M}) -> I+J*A+K*A*B+L*A*B*C+M*A*B*C*D;
-    index(_,_) -> error(unimplemented).
-        
+volume(Vec) -> 
+    [_|Vs] = tuple_to_list(Vec),
+    lists:foldl(fun erlang:'*'/2, 1, Vs).
+index({_,_},{_,I}) -> I;
+index({_,A,_},{_,I,J}) -> I+J*A; % I'm sure compiler optimizes these~~
+index({_,A,B,_},{_,I,J,K}) -> I+J*A+K*A*B;
+index({_,A,B,C,_},{_,I,J,K,L}) -> I+J*A+K*A*B+L*A*B*C;
+index({_,A,B,C,D,_},{_,I,J,K,L,M}) -> I+J*A+K*A*B+L*A*B*C+M*A*B*C*D;
+index(_Size,_Point) -> error(unimplemented).
+    
